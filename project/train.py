662ed0f06b85b80827ee51a0eb892713e682ab94
Jonny2k4 <136419104+Jonny2k4@users.noreply.github.com>
2023-07-04 23:50:52 +0600

    Update train.py

--- project/train.py
+++ project/train.py
@@ -12,10 +12,6 @@ import tensorflow_addons as tfa
 
 tf.config.optimizer.set_jit("True")
 
-
-# Parsing variable ctrl + /
-# ----------------------------------------------------------------------------------------------
-
 parser = argparse.ArgumentParser()
 parser.add_argument("--root_dir")
 parser.add_argument("--dataset_dir")
@@ -27,22 +23,13 @@ parser.add_argument("--gpu")
 
 args = parser.parse_args()
 
-
-# Set up train configaration
-# ----------------------------------------------------------------------------------------------
 config = get_config_yaml('project/config.yaml', vars(args)) 
 create_paths(config, test = False)
 
-
-# setup gpu
-# ----------------------------------------------------------------------------------------------
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = config["gpu"]
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 
-
-# Print Experimental Setup before Training
-# ----------------------------------------------------------------------------------------------
 print("Model = {}".format(config['model_name']))
 print("Epochs = {}".format(config['epochs']))
 print("Batch Size = {}".format(config['batch_size']))
@@ -50,31 +37,19 @@ print("Preprocessed Data = {}".format(os.path.exists(config['train_dir'])))
 print("Class Weigth = {}".format(str(config['weights'])))
 print("Experiment = {}".format(str(config['experiment'])))
 
-# Dataset
-# ----------------------------------------------------------------------------------------------
 train_dataset, val_dataset = get_train_val_dataloader(config)
 
-
-# Metrics
-# ----------------------------------------------------------------------------------------------
 metrics = list(get_metrics(config).values())  # [list] required for new model
 custom_obj = get_metrics(config) # [dictionary] required for transfer learning & fine tuning
 
-# Optimizer
-# ----------------------------------------------------------------------------------------------
 learning_rate = 0.001
 weight_decay = 0.0001
 adam = tfa.optimizers.AdamW(
     learning_rate=learning_rate, weight_decay=weight_decay)
 
-# Loss Function
-# ----------------------------------------------------------------------------------------------
 loss = tf.keras.losses.BinaryCrossentropy(from_logits=True) # required for new model
 custom_obj['loss'] = focal_loss() # required for transfer learning/fine-tuning
 
-# Compile
-# ----------------------------------------------------------------------------------------------
-# transfer learning
 if (os.path.exists(os.path.join(config['load_model_dir'], config['load_model_name']))) and config['transfer_lr']:
     print("Build model for transfer learning..")
     # load model and compile
@@ -89,22 +64,16 @@ else:
     if (os.path.exists(os.path.join(config['load_model_dir'], config['load_model_name']))):
         print("Resume training from model checkpoint {}...".format(
             config['load_model_name']))
-        # load model and compile
         model = load_model(os.path.join(
             config['load_model_dir'], config['load_model_name']), custom_objects=custom_obj, compile=True)
 
-    # new model
     else:
         model = get_model(config)
         model.compile(optimizer=adam, loss=loss, metrics=metrics)
 
-# Callbacks
-# ----------------------------------------------------------------------------------------------
 loggers = SelectCallbacks(val_dataset, model, config)
 model.summary()
 
-# Fit
-# ----------------------------------------------------------------------------------------------
 t0 = time.time()
 history = model.fit(train_dataset,
                     verbose=1,
