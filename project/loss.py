9974804a06dfaa9c2d4583481ac1ca640244a051
Jonny2k4 <136419104+Jonny2k4@users.noreply.github.com>
2023-07-04 23:44:20 +0600

    Update loss.py

--- project/loss.py
+++ project/loss.py
@@ -3,29 +3,15 @@ from tensorflow.keras import backend as K
 import tensorflow as tf
 import segmentation_models as sm
 
-# Setting framework
-
 sm.set_framework('tf.keras')
 sm.framework()
 
-
 def loss():
-    '''
-    Summary:
-        This functions get the total loss calculating using dice loss and focal loss
-    Arguments:
-        None
-    Return:
-        Total loss (segmentation_models.base.objects.SumOfLosses object)
-    '''
     weights = [0.1666, 0.1666, 0.1666, 0.1666, 0.1666, 0.1666]
     dice_loss = sm.losses.DiceLoss(class_weights=weights)
     focal_loss = sm.losses.CategoricalFocalLoss()
     total_loss = dice_loss + (1 * focal_loss)
     return total_loss
-
-# keras.losses.custom_loss = loss()
-
 
 def focal_loss(alpha=0.25, gamma=2):
     def focal_loss_with_logits(logits, targets, alpha, gamma, y_pred):
